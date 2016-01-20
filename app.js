52434c41f87964d84add78812cfa47ca4bfb60b5
hyeok <zealot71@naver.com>
2016-01-20 18:29:53 +0900

    board and member add commit

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 // import modules
 var express = require('express');
-var path = require('path');
 var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
+
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var async = require('async');
+
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // connect database
 mongoose.connect(process.env.MONGO_DB);
@@ -26,27 +33,233 @@ var postSchema = mongoose.Schema({
 
 var Post = mongoose.model('post', postSchema);
 
+var userSchema = mongoose.Schema({
+  email: {type:String, required:true, unique:true},
+  nickname: {type:String, required:true, unique:true},
+  password: {type:String, required:true},
+  createdAt: {type:Date, default:Date.now}
+});
+
+var User = mongoose.model('user', userSchema);
+
+
 // view setting
 app.set("view engine", 'ejs');
 
 // set middlewares
 app.use(express.static(path.join(__dirname + '/public')));
+app.use(bodyParser.json()); // body json으로 파싱
+app.use(bodyParser.urlencoded({extended:true}));  // url 데이터를 받기 위해
+app.use(methodOverride("_method"));
 
-// set route
-app.get('/posts', function(req, res) {
-  Post.find({}, function(err,posts) {
-    if(err) return res.json({success: false, message: err});
-    res.json({success:true, data:posts});
+app.use(flash());
+app.use(session({secret:'MySecret'}));  // 로그인 유지, 암호화할 때 사용하는 비밀 해시키
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeuser(function(user, done) { // session 생성시 어떤 정보를 저장할지 설정 ( DB의 ID )
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) { // session 으로부터 개체를 어떻게 가져올지 설정 ( id를 넘겨받아 DB에서 user를 찾고, user를 가져옴 )
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
-}); // index
+});
+
+var LocalStrategy = require('passport-local').Strategy;
+passport.use('local-login', new LocalStrategy( {
+  usernameField:"email",
+  passwordField:"password",
+  passReqToCallback:true
+}, function(req, email, password, done) {
+  User.findOne({ 'email':email}, function(err, user) {
+    if(err) return done(err);
+
+    if(!user) {
+      req.flash("email", req.body.email);
+      return done(null, false, req.flash('loginError', 'No user found.'));
+    }
+
+    if(user.password != password) {
+      req.flash("email", req.body.email);
+      return done(null, false, req.flash('loginError', 'Password does not Match.'));
+    }
+
+    return done(null, user);
+  });
+}));
 
 
-app.post('/posts', function(req, res) {
-  Post.create(req.body.post, function(err, post) {
+// set home route
+app.get('/', function(req, res) {
+  res.redirect('/posts');
+});
+app.get('/login', function(req, res) {
+  res.render('login/login', {email:req.flash("email")[0], loginError:req.flash('loginError')});
+});
+app.post('/login', function(req, res, next) {
+  req.flash("email");
+  if(req.body.email.length === 0 || req.body.password.length === 0) {
+    req.flash("email", req.body.email);
+    req.flash("loginError", "Please enter both email and password");
+    res.redirect('/login');
+  } else {
+    next();
+  }
+}, passpord.authenticate('local-login', {
+  successRedirect:'/posts',
+  failureRedirect:'/login',
+  failureFlash:true
+})
+);
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+
+// set user routes
+app.get('/users/new', function(req, res) {
+  res.render('users/new', {
+    formData: req.flash('formData')[0],
+    emailError: req.flash('emailError')[0],
+    nicknameError: req.flash('nicknameError')[0],
+    passwordError: req.flash('password')[0]
+  });
+}); // new
+app.post('users', checkUserRegValidation, function(req, res, next) {
+  User.create(req.body.user, function(err, user) {
     if(err) return res.json({success:false, message:err});
-    res.json({success:true, data:post});
+    res.redirect('/login');
+
   });
 }); // create
+
+app.get('/users/:id', function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if(err) return res.json({success:false, message:err});
+    res.render("users/show", {user: user});
+  });
+}); // show
+
+app.get('/users/:id/edit', function(req, res) {
+  User.findById(req.params.id, function(err, user) {
+    if(err) return res.json({success:false, message:err});
+    res.render("users/edit", {
+      user : user,
+      formData : req.flash('formData')[0],
+      emailError : req.flash('emailError')[0],
+      nicknameError : req.flash('nicknameError')[0],
+      passwordError : req.flash('passwordError')[0]
+    });
+  });
+}); // edit
+
+
+app.put('/users/:id', checkUserRegValidation, function(req, res) {
+  User.findById(req.params.id, req.body.user, function(err, user) {
+    if(err) return res.json({success:false, message:err});
+    if(req.body.user.password == user.password) {
+      if(req.body.user.newPassword) {
+        req.body.user.password-req.body.user.newPassword;
+      } else {
+        delete req.body.user.password;
+      }
+      User.findByIdAndUpdate(req.params.id, req.body.user, function(err,user) {
+        if(err) return res.json({success:false, message:err});
+        res.redirect('/users/', req.params.id);
+      });
+    } else {
+      req.flash("formData", req.body.user);
+      req.flash("passwordError", "- Invalid password");
+      res.redirect("/users/"+req.params.id+"/edit");
+    }
+  });
+}); // update
+
+
+// set posts route
+app.get('/posts', function(req, res) {
+  Post.find({}).sort('-createdAt').exec(function(err, posts) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/index", {data:posts, user:req.user});
+  });
+}); // index
+app.get('/posts/new', function(req, res) {
+  res.render("posts/new");
+}); // new
+app.post('/posts', function(req, res) {
+  console.log(req.body);
+  Post.create(req.body.post, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.redirect('/posts');
+  });
+}); // create
+app.get('/posts/:id', function(req, res) {
+  Post.findById(req.params.id, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/show", {data:post});
+  });
+}); // show
+app.get('/posts/:id/edit', function(req, res) {
+  Post.findById(req.params.id, function(err, post) {
+    if(err) return res.json({success:false, message:err});
+    res.render("posts/edit", {data:post});
+  });
+}); //edit
+app.put('/posts/:id', function(req, res) {
+  req.body.post.updatedAt = Date.now();
+  Post.findByIdAndUpdate(req.params.id, req.body.post, function (err, post) {
+    if(err) return res.json({succes:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); // update
+app.delete('/posts/:id', function(req, res) {
+  req.body.
+  Post.findByidAndRemove(req.params.id, function(err, post) {
+    if(err) return res.json ({succes:false, message:err});
+    res.redirect('/posts/'+req.params.id);
+  });
+}); // destory
+
+function checkUserRegValidation(req, res, next) {
+  var isValid = true;
+
+  async.waterfall(
+    [function(callback) {
+      User.findOne({email: req.body.user.email, _id: {$ne: mongoose.Types.ObjectId(req.params.id)}},
+      function(err, user) {
+        if(user) {
+          isValid = false;
+          req.flash("emailError", "- This email is already resistered.");
+        }
+        callback(null, isValid);
+      }
+    );
+  }, function(isValid, callback) {
+    User.findOne({nickname: req.body.user.nickname, _id: {$ne: mongoose.Types.ObjectId(req.params.id)}},
+      function(err,user) {
+        if(user) {
+          isValid = false;
+          req.flash("nicknameError", "- This nickname is already resistered.");
+        }
+        callback(null, isValid);
+      }
+  );
+}], function(err, isValid) {
+        if(err) return res.json({success:false, message:err});
+        if(isValid) {
+          return next();
+        } else {
+          req.flash("formData", req.body.user);
+          res.redirect("back");
+        }
+      }
+    );
+  }
+
+
 
 // start server
 app.listen(3000, function() {
